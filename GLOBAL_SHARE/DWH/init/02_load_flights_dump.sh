9070de4de00ef7e6fab9620f8da19247a5d8381e
Vova Kh <VKHDK@yandex.ru>
2024-04-10 09:45:24 +0300

    working version

--- GLOBAL_SHARE/DWH/init/02_load_flights_dump.sh
+++ GLOBAL_SHARE/DWH/init/02_load_flights_dump.sh
@@ -1,3 +1,3 @@
 # load data into the database from dump
 current_dump="source_flights_dump.sql"
-psql -c 'SET search_path TO source_db;' -U dwh_postgres -d source_db  -f /var/lib/postgresql/dumps/$current_dump
+psql -c 'SET search_path TO source_schema;' -U dwh_postgres -d source_db  -f /var/lib/postgresql/dumps/$current_dump
